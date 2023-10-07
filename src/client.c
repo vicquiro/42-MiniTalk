18833080437b0e157360a706ecbf93b78e68b0aa
vicquiro <victorky98@gmail.com>
2023-10-07 18:53:27 +0200

    Done simple comunication between both processes

--- src/client.c
+++ src/client.c
@@ -1,9 +1,9 @@
 #include "../includes/minitalk.h"
-//Read PID and message from CLI
+// Read PID and message from CLI
 
 /* Iterates over each char and convert to 8bits
 / and send each bit to server
-/ 
+/
 / In send signal function we send 0 or 1 to server
 / SIGURS1 = 1
 / SIGURS2 = 0
@@ -12,57 +12,58 @@
 /**
  * In send_signal function, after send each bit
  * we wait (usleep) a few micro-seconds
-*/
+ */
 
-void send_to_server(int pid_server, unsigned char element){
+void send_to_server(int pid_server, unsigned char element)
+{
     int i;
-    
-    printf("Sending: %c\n", element) ;
+
+    printf("Element: %d\n", element); //TODO: Remove this
     i = 7;
     while (i >= 0)
     {
-     
-        if(((element >> i) & 1) == 1)
+        if (((element >> i) & 1) == 1)
         {
-            //printf("1");
-            kill(pid_server, SIGUSR1); //Send 1
+            printf("Bit: %d\n", 1);
+            kill(pid_server, SIGUSR1); // Send 1
         }
         else
         {
-            //printf("0");
-            kill(pid_server, SIGUSR2); //Send 0
+            printf("Bit: %d\n", 0);
+            kill(pid_server, SIGUSR2); // Send 0
         }
-        printf("\n");
-        usleep(1000);
+
         i--;
+        usleep(42);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    pid_t pid_server;
+
+    int i;
+    const char *message;
+    if (argc != 3)
+    {
+        // TODO: Change printf to ft_printf
+        printf("Error: ./client [PID_SERVER] [MESSAGE]\n");
+        return (0);
     }
 
-}
-
-int	main(int argc, char **argv)
-{
-	int		pid_server;
-	
-	int		i;
-
-	if (argc != 3)
-	{
-		//TODO: Change printf to ft_printf
-		printf("Error: ./client [PID_SERVER] [MESSAGE]\n");
-		return (0);
-	}
-
-	pid_server = atoi(argv[1]); //Change this to ft_atoi
+    pid_server = atoi(argv[1]); // Change this to ft_atoi
 
     printf("PID_SERVER: %d\n", pid_server);
-    printf("MESSAGE: %s\n", argv[2]);
-	i = 0;
-   
-	while (argv[2][i]!= '\0')
-	{
-		send_to_server(pid_server, argv[2][i]);
-		i++;
-	}
-	return (0);
-}
+    message = argv[2];
+    printf("MESSAGE: %s\n", message);
 
+    i = 0;
+
+    while (message[i] != '\0')
+    {
+        send_to_server(pid_server, message[i]);
+        i++;
+    }
+    send_to_server(pid_server, '\0');
+    return (0);
+}
--- src/server.c
+++ src/server.c
@@ -1,33 +1,67 @@
 #include "../includes/minitalk.h"
+
+#define END_TRANSMISSION '\0'
+// void convertToBinary(unsigned a)
+// {
+// 	if (a > 1)
+// 		convertToBinary(a / 2);
+// 	printf("%d", a % 2);
+// }
 /**
  * SINGAL_HANDLER funtion:
- * 
+ *
  * */
-void	handler_signal(int signal)
+void signal_handler(int signal)
 {
-	if (signal == SIGUSR1)
-		printf("1");
-	else if (signal == SIGUSR2)
-		printf("0");
+	static unsigned char current_char;
+	static int bit_index;
+
+	// convertToBinary(current_char);
+	// printf("\n");
+	// printf("OR\n");
+	// convertToBinary(signal == SIGUSR1);
+	// printf("\n");
+	current_char = current_char | (signal == SIGUSR1);
+	// printf("__________________\n");
+	// convertToBinary(current_char);
+	// printf("\n");
+	
+	bit_index++;
+	if (bit_index == 8)
+	{
+		if (current_char == END_TRANSMISSION)
+			printf("\n"); // TODO: Change this to ft_printf
+		else
+			printf("%c", current_char); // TODO: Change this to ft_printf
+		bit_index = 0;
+		current_char = 0;
+	}
+	else
+	{
+		current_char <<= 1;
+		// printf("-");
+		// convertToBinary(current_char);
+		// printf("\n");
+	}
+
 }
 
 /**
  * Main function:
  *  Display PID in stdout
  *  Infinite Loop waiting for signals
- *     - 
-*/
-int	main(void)
+ *     -
+ */
+int main(void)
 {
-	pid_t	pid;
+	pid_t pid;
 
 	pid = getpid();
-	printf("Server PID: %d\n", pid); //Change this to ft_printf
+	printf("Server PID: %d\n", pid); // Change this to ft_printf
 
-	signal(SIGUSR1, handler_signal);
-	signal(SIGUSR2, handler_signal);
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
 	while (1)
 		pause();
 	return (0);
 }
-
